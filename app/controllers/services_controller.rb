16f2cf6c5e317bbf8a8692351ec98a101d1ab9b6
Umer Saqib <umersaqib160@hotmail.com>
2016-06-08 18:28:01 +0200

    remove clutter

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -11,17 +11,3 @@ class ServicesController < ApplicationController
 
 
 end
-
-<div class="share-links">
-  <%= link_to "https://www.facebook.com/umersaqib160", class: "share-link bg-facebook" do  %>
-    <i class="fa fa-facebook"></i>
-  <% end %>
-  <%= link_to "", class: "share-link bg-twitter" %>
-    <i class="fa fa-twitter"></i>
-  </a>
-  <%= link_to "", class:"share-link bg-email" %>
-    <i class="fa fa-envelope-o"></i>
-  </a>
-  <%= link_to "", class:"share-link bg-text" %>
-    <i class="fa fa-mobile"></i>
-</div>
