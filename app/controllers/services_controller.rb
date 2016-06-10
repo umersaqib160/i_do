5e804a9e2bb08d142f4c19986c7a3c8be0b936c2
Umer Saqib <umersaqib160@hotmail.com>
2016-06-10 12:32:58 +0200

    WIP fixing the services index page with the search box

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,58 +6,63 @@ class ServicesController < ApplicationController
       marker.lng user.longitude
 
     end
-  def index
-    @services = Service.all
+    def index
+      @services = Service.all
+
+    end
+
+    def show
+      @service = Service.find(params[:id])
+
+
+    end
+
+    def new
+      @service = Service.new
+    end
+
+    def edit
+      @service.find(params[:id])
+    end
+
+    def update
+      @service.find(params[:id])
+
+    end
+
+    def create
+      @service = Service.new(service_params)
+      @service.save!
+      redirect_to service_path(@service)
+    end
+    def destroy
+      @service = Service.find(params[:id])
+      @service.destroy
+
+      redirect_to service_path
+
+    end
+
+    def search
+      @searched_term = params[:service_name]
+      @service_providers = Service.where(name: @searched_term)
+    end
+
+    private
+
+    def service_params
+      params.require(:service).permit(:name, :supplier_id)
+    end
+
+    def self.search(search)
+      if search
+        find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+      else
+        find(:all)
+      end
+    end
+
+
+
 
   end
-
-  def show
-    @service = Service.find(params[:id])
-
-
-  end
-
-  def new
-    @service = Service.new
-  end
-  def edit
-    @service.find(params[:id])
-
-  end
-
-  def update
-    @service.find(params[:id])
-
-  end
-
-  def create
-    @service = Service.new(service_params)
-    @service.save!
-    redirect_to service_path(@service)
-  end
-def destroy
-  @service = Service.find(params[:id])
-  @service.destroy
-
-  redirect_to service_path
-
-end
-
-private
-
-def service_params
-  params.require(:service).permit(:name, :supplier_id)
-end
-
-def self.search(search)
-  if search
-    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-  else
-    find(:all)
-  end
-end
-
-
-
-
-end
