2e76f0a93d01ce7a0765010370db98f151b63fda
Umer Saqib <umersaqib160@hotmail.com>
2016-06-10 14:08:37 +0200

    search bar working fine but still last touch to be done

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,13 +1,20 @@
 class ServicesController < ApplicationController
 
     # Let's DYNAMICALLY build the markers for the view.
-    @markers = Gmaps4rails.build_markers(@users) do |user, marker|
-      marker.lat user.latitude
-      marker.lng user.longitude
+    # @markers = Gmaps4rails.build_markers(@users) do |user, marker|
+    #   marker.lat user.latitude
+    #   marker.lng user.longitude
 
-    end
+    # end
+
     def index
-      @services = Service.all
+      if params[:search_term]
+
+        @searched_term = params[:search_term]
+        @services = Service.where(name: @searched_term)
+      else
+        @services = Service.all
+      end
 
     end
 
@@ -43,24 +50,12 @@ class ServicesController < ApplicationController
 
     end
 
-    def search
-      @searched_term = params[:service_name]
-      @service_providers = Service.where(name: @searched_term)
-    end
-
     private
 
     def service_params
       params.require(:service).permit(:name, :supplier_id)
     end
 
-    def self.search(search)
-      if search
-        find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-      else
-        find(:all)
-      end
-    end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  post 'services' => 'services#search'
+  post 'services' => 'services#index'
 
 resources :services, only: [:index, :show] do
   resources :reservations, only: [:new, :create]
